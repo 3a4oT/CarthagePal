7e86d6cc84684d1c05807e5fd108b44160fcc9aa
3a4oT <petro.rovenskyy@gmail.com>
2020-09-15 23:38:32 +0300

    Catch output

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 #!/bin/zsh
 
-carthage update --new-resolver --platform iOS
+carthage update --new-resolver --platform iOS &
+
+BACK_PID=$!
+
+while kill -0 $BACK_PID ; do
+    echo "Process is still active..."
+    sleep 1
+done
 
 # cleanup
 rm -rf Carthage/Checkouts/
-rm -rf Carthage/Build/macOS/
+rm -rf Carthage/Build/Mac/
 rm -rf Carthage/Build/tvOS/
 rm -rf Carthage/Build/watchOS/
 
